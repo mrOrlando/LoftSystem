67e8895b87036100e2efbbab651e71966746d690
mrOrlando <juriy88@mail.com>
2019-03-15 21:33:43 +0300

    index.html file is returned to any request.

--- server/app.js
+++ server/app.js
@@ -8,6 +8,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ type: 'text/plain' })); // fix bug from frontent
 app.use('/', require('./routes/index'));
+app.use('*', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const controllerHome = require('../controllers/home');
 const controllerApi = require('../controllers/api');
 
-router.get('/', controllerHome.get);
 
 router.post('/api/saveNewUser', controllerApi.saveNewUser);
 router.post('/api/login', controllerApi.login);
